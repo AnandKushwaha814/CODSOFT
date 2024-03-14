3a65b6dde7273cc341cd220b7f9d32f67f768298
Anand Kumar Kushwaha <127625667+AnandKushwaha814@users.noreply.github.com>
2024-03-14 15:37:02 +0530

    Delete main1.js

--- main1.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text123", {
-    strings: ["Frontend Developer", "Youtuber", "Web Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
